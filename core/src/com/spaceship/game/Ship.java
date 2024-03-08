7598a02f0c5545d5e1414938029696c6490a2168
agb727387 <107209012+agb727387@users.noreply.github.com>
2024-03-08 15:33:13 -0600

    Remove default java project comments

--- core/src/com/spaceship/game/Ship.java
+++ core/src/com/spaceship/game/Ship.java
@@ -1,12 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.spaceship.game;
-/**
- *
- * @author andrewgarcia
- */
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
